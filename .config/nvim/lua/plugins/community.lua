c808cf5dd277213abc11122b81a03a4c778e73b7
Charles Sena <charlessena@gokeitecnologia.com.br>
2026-05-08 11:11:21 -0300

    Astro nvim plugins update

--- .config/nvim/lua/plugins/community.lua
+++ .config/nvim/lua/plugins/community.lua
@@ -3,14 +3,14 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-  { import = "astrocommunity.note-taking.obsidian-nvim"},
   { import = "astrocommunity.colorscheme.nordic-nvim" },
-  { import = "astrocommunity.recipes.cache-colorscheme" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.test.vim-test"},
+  { import = "astrocommunity.note-taking.obsidian-nvim"},
   { import = "astrocommunity.pack.astro"},
   { import = "astrocommunity.pack.laravel" },
+  { import = "astrocommunity.pack.markdown"},
   { import = "astrocommunity.pack.vue"},
-  { import = "astrocommunity.pack.markdown"}
+  { import = "astrocommunity.recipes.cache-colorscheme" },
+  { import = "astrocommunity.test.vim-test"}
 }
